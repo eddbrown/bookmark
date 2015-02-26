80922601e8f4db6b63aca0e02504a88a84aae606
Edward Brown <edward.brown.london@gmail.com>
2015-02-26 16:29:10 +0000

    signs in

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "User sings up" do
+feature "User signs up" do
 
   scenario "when being a new user visiting the site" do
     expect{ sign_up }.to change(User, :count).by 1
@@ -31,4 +31,36 @@ feature "User sings up" do
     click_button "Sign up"
   end
 
+end
+
+
+feature 'User signs in' do
+
+  before(:each) do
+    User.create(:email => "test@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario "with correct credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'test')
+    expect(page).to have_content("Welcome, test@test.com")
+  end
+
+  scenario "with incorrect credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'wrong')
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+
+  def sign_in(email, password)
+    visit '/sessions/new'
+    fill_in 'email', :with => email
+    fill_in 'password', :with => password
+    click_button 'Sign in'
+  end
+  
 end
