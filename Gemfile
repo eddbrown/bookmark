360dcb6112e187a206877e70f7ffdaa04db28420
Edward Brown <edward.brown.london@gmail.com>
2015-02-26 13:04:22 +0000

    added some FLASH aaaaahaaaaaaah

--- Gemfile
+++ Gemfile
@@ -5,12 +5,11 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'database_cleaner'
 gem 'bcrypt-ruby'
+gem 'rack-flash3'
 
 group :development, :test do
   gem 'capybara'
-  gem 'cucumber'
-  gem 'cucumber-sinatra'
   gem 'launchy'
   gem 'rspec'
-  gem 'shotgun'
 end
+ 
--- app/server.rb
+++ app/server.rb
@@ -4,12 +4,14 @@ require './app/helpers/application.rb'
 require './lib/link'
 require './lib/tag'
 require './lib/user'
+require 'rack-flash'
 
 class BookmarkManager < Sinatra::Base
 
   require_relative 'data_mapper_setup'
   enable :sessions
   set :session_secret, 'super secret'
+  use Rack::Flash
 
   helpers BookmarkUtils
 
@@ -35,15 +37,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
-    erb :new_user
+    @user = User.new
+    erb :"users/new"
   end
 
   post '/users' do
-    user = User.create(:email => params[:email],
+    @user = User.new(:email => params[:email],
                 :password => params[:password],
                 :password_confirmation => params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to '/'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to '/'
+    else
+      flash[:notice] = "Sorry, your passwords don't match"
+      erb :"users/new"
+    end
   end
 
   #start the server if ruby file executed directly
